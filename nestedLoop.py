453d762b226635a6333c31678b41ecda5378503f
Crystal Broach <crysland@gmail.com>
2020-09-26 13:10:21 -0500

    looping in python

---
+++ nestedLoop.py
@@ -0,0 +1,14 @@
+# for x in range(4):
+#     for y in range(3):
+#         print(f'({x}, {y})')
+
+numbers = [1, 1, 1, 1, 5]
+
+for number in numbers:
+    string = ''
+    for count in range(number):
+        string += 'X'
+    print(string)
+
+
+
