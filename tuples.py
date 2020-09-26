5537d166c0462288edc48e1af395a9eb7c77c79c
Crystal Broach <crysland@gmail.com>
2020-09-26 13:48:37 -0500

    tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+# numbers = (1, 2, 3)
+# numbers[0] = 10
+# print(numbers[0])
+
+coordinates = (1, 2, 3)
+
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+
+x, y, z = coordinates
