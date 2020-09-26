21b8dc3de5781cbd11d78a5f040d6ab285e3983a
Crystal Broach <crysland@gmail.com>
2020-09-26 15:00:12 -0500

    modified emoji converter to a function

--- emojiConverter.py
+++ emojiConverter.py
@@ -1,14 +1,19 @@
+def emoji_converter(message_input):
+    words = message_input.split(' ')
+
+    emojis = {
+        ":)": "🙂",
+        ":(": "☹"
+    }
+
+    output = ""
+
+    for word in words:
+        output += emojis.get(word, word) + " "
+
+    return output
+
+
 message = input(">")
-words = message.split(' ')
 
-emojis = {
-    ":)": "🙂",
-    ":(": "☹"
-}
-
-output = ""
-
-for word in words:
-    output += emojis.get(word, word) + " "
-
-print(output)
+print(emoji_converter(message))
