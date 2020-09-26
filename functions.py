e0abe3d5e4759fb6772264b594c35dad61a52a9b
Crystal Broach <crysland@gmail.com>
2020-09-26 14:53:28 -0500

    functions and return statements

---
+++ functions.py
@@ -0,0 +1,16 @@
+# def greet_user(first_name, last_name):
+#     print(f'Hi {first_name} {last_name}!')
+#     print('Welcome aboard')
+#
+#
+# print("Start")
+# greet_user(last_name="Smith", first_name="John")
+# print("Finish")
+
+
+def square(number):
+    return number * number
+
+
+print(square(3))
+
